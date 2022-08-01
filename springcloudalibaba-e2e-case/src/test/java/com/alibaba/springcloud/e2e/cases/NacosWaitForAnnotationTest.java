af364623bf84af7f88bc411c4c36e1ae290d7bba
windwheel <yuluoxinsheng@gmail.com>
2022-08-01 09:32:34 +0800

    修复junit5+ testcontainer自动化测试

---
+++ springcloudalibaba-e2e-case/src/test/java/com/alibaba/springcloud/e2e/cases/NacosWaitForAnnotationTest.java
@@ -0,0 +1,25 @@
+package com.alibaba.springcloud.e2e.cases;
+
+import com.alibaba.springcloud.e2e.core.SpringCloudAlibaba;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+@SpringCloudAlibaba(composeFiles = "docker/nacos-compose-test.yml", serviceName = "nacos-standalone")
+public class NacosWaitForAnnotationTest {
+    
+    
+    @BeforeAll
+    public static void setUp() {
+    
+    }
+    
+    @Test
+    @Order(10)
+    public void testWaitOnListeningPort() {
+       LOGGER.info("nacos已经启动....");
+    }
+    
+}
